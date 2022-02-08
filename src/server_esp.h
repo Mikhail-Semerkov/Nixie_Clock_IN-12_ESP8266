e702da6a4a2ce1aa0acd88d79aefa8584c7a89d6
Mikhail-Semerkov <mikhail.semerkov@mail.ru>
2022-02-08 23:17:56 +0300

    Home

--- src/server_esp.h
+++ src/server_esp.h
@@ -40,15 +40,88 @@ void udp_synch()
   }
 }
 
+const char *htmlfile = "/index.htm";
+
+bool loadFromSpiffs(String path)
+{
+  String dataType = "text/plain";
+  if (path.endsWith("/"))
+    path += "index.htm";
+  if (path.endsWith(".src"))
+    path = path.substring(0, path.lastIndexOf("."));
+  else if (path.endsWith(".html"))
+    dataType = "text/html";
+  else if (path.endsWith(".htm"))
+    dataType = "text/html";
+  else if (path.endsWith(".css"))
+    dataType = "text/css";
+  else if (path.endsWith(".js"))
+    dataType = "application/javascript";
+  else if (path.endsWith(".png"))
+    dataType = "image/png";
+  else if (path.endsWith(".gif"))
+    dataType = "image/gif";
+  else if (path.endsWith(".jpg"))
+    dataType = "image/jpeg";
+  else if (path.endsWith(".ico"))
+    dataType = "image/x-icon";
+  else if (path.endsWith(".xml"))
+    dataType = "text/xml";
+  else if (path.endsWith(".pdf"))
+    dataType = "application/pdf";
+  else if (path.endsWith(".zip"))
+    dataType = "application/zip";
+  File dataFile = SPIFFS.open(path.c_str(), "r");
+  if (server.hasArg("download"))
+    dataType = "application/octet-stream";
+  if (server.streamFile(dataFile, dataType) != dataFile.size())
+  {
+  }
+  dataFile.close();
+  return true;
+}
+
+void handleWebRequests()
+{
+  if (loadFromSpiffs(server.uri()))
+    return;
+  String message = "File Not Detected\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++)
+  {
+    message += " NAME:" + server.argName(i) + "\n VALUE:" + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  Serial.println(message);
+}
+
+void handleRoot()
+{
+  server.sendHeader("Location", "/index.html", true);
+  server.send(302, "text/plane", "");
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
 void setup_server()
 {
-  httpUpdater.setup(&server, "/");
+  SPIFFS.begin();
+  server.on("/", handleRoot);
+  server.onNotFound(handleWebRequests);
+
+  httpUpdater.setup(&server, "/update");
   server.on("/json", HTTP_GET, []()
             {
                   String
                   json = "{\n";
 
-                  json += "\"chip_id\":" + String("\"") + ESP.getChipId() + String("\", \n");
+                  json += "\"nixie_time\":" + String("\"") + Time_str + String("\", \n");
                   json += "\"free_heap\":" + String("\"") + ESP.getFreeHeap() + String("\"");
 
                   json += "\n}";
