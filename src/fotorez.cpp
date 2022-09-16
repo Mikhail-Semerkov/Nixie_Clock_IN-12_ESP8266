bcef42d7bcc7315f24f5f5e2dc82b270f68e1276
Mikhail <mikhail.semerkov@miatech.ru>
2022-09-16 18:45:02 +0300

    fotorez current

--- src/fotorez.cpp
+++ src/fotorez.cpp
@@ -1,10 +1,12 @@
 #include "fotorez.h"
 #include "trace.h"
 
+#define GAIN_ADC 2
+
 uint16_t get_adc(void)
 {
     uint16_t adc_value;
     adc_value = analogRead(A0);
     adc_value = map(adc_value, 0, 1024, 0, 10);
-    return adc_value;
+    return adc_value = adc_value + GAIN_ADC;
 }
