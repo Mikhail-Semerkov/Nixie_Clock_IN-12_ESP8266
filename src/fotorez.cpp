0308fd392e59384d9313b951959bf73ea89f5749
Mikhail <mikhail.semerkov@miatech.ru>
2022-09-18 23:19:28 +0300

    brightness timeout

--- src/fotorez.cpp
+++ src/fotorez.cpp
@@ -5,8 +5,24 @@
 
 uint16_t get_adc(void)
 {
+    static uint16_t timer_value = 1;
+    static unsigned long timer;
     uint16_t adc_value;
     adc_value = analogRead(A0);
-    adc_value = map(adc_value, 0, 1024, 0, 10);
-    return adc_value = adc_value + GAIN_ADC;
+    adc_value = map(adc_value, 0, 1024, 1, 10);
+
+    if (timer_value != adc_value)
+    {
+        if (millis() - timer > 3000)
+        {
+            timer = millis();
+            timer_value = adc_value;
+        }
+    }
+    else
+    {
+        timer = millis();
+    }
+
+    return timer_value;
 }
